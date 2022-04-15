b782a49ec3304bdc8290ecf2e91ab4f137166d1e
Navneet234 <103722293+Navneet234@users.noreply.github.com>
2022-04-15 10:27:57 +0530

    1 commit

---
+++ add_prac.java
@@ -0,0 +1,35 @@
+class addition_prac
+{
+public static void main(String arr[])
+{
+try{
+int a=10,b=20,add;
+add=a+b;
+
+System.out.println("Addition is:"+add);
+}
+catch(ArithmeticException p)
+{
+System.out.println(p);
+}
+}
+}
+
+/*public class clsnotfound{
+
+public static void main(String args[])
+{
+
+try {
+
+Class.forName("hello");
+}
+
+catch (ClassNotFoundException ex) {
+
+System.out.println(ex);
+}
+System.out.println("Working");
+}
+}
+*/
